48b5dc9a9a6f81f930376d8c51032c4e9aac5898
DemetrioPadre <demetriopadre@gmail.com>
2024-04-12 23:49:00 +0200

    bonus done

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -2,6 +2,12 @@
 export default {
     props: {
         project: Object
+    },
+    computed: {
+        abstract() {
+            const content = this.project.content;
+            return content.lenght > 20 ? content.substring(0, 20) + '...' : content;
+        }
     }
 };
 </script>
@@ -11,10 +17,16 @@ export default {
         <div class="card h-100 mb-5">
             <img v-if="project.image" :src="project.image" class="card-img-top">
             <div class="card-body">
-                <p class="card-text">{{ project.id }}</p>
+                <span :style="'background-color:' + project.type.color" class=" badge mt-2 mb-4">{{
+                project.type.label }}</span>
+                <p class=" card-text">{{ project.id }}</p>
                 <h5 class="card-title">{{ project.title }}</h5>
-                <p class="card-text">{{ project.content }}</p>
+                <p class="card-text">{{ abstract }}</p>
                 <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+            <div class="card-footer">
+                <span :style="'background-color:' + technology.color" class=" badge me-3"
+                    v-for=" technology in project.technologies">{{ technology.label }}</span>
             </div>
         </div>
     </div>
--- src/components/ProjectList.vue
+++ src/components/ProjectList.vue
@@ -5,23 +5,41 @@ import ProjectCard from './ProjectCard.vue';
 export default {
     data() {
         return {
-            store
+            store,
+            pagination: [],
         };
+    },
+    methods: {
+        fetchProjects(endpoint = api.baserUrl + 'project') {
+            axios.get(endpoint).then((response) => {
+                store.projects = response.data.data;
+                this.pagination = response.data.links;
+            });
+        },
     },
     components: { ProjectCard },
     created() {
-        axios.get(api.baserUrl + 'project').then((response) => {
-            store.projects = response.data.data;
-        });
+        this.fetchProjects();
     }
+
 
 };
 </script>
 <template>
 
-    <div class="row row-cols-4 g-3">
+    <div class="row row-cols-4 g-3 ">
         <project-card v-for="project in store.projects" :project="project" />
     </div>
+    <nav aria-label="Page navigation example" class="mt-4">
+        <ul class="pagination">
+            <li @click="fetchProjects(link.url)" class="page-item" :class="link.active ? 'active' : ''"
+                v-for="link in pagination">
+                <a class="page-link" href="#" v-html="link.label"></a>
+            </li>
+
+        </ul>
+    </nav>
+
 
 </template>
 <style lang="scss"></style>
