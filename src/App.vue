f8d579bd27edd5a37124c1e3f6f3932a323ea0b1
DemetrioPadre <demetriopadre@gmail.com>
2024-04-12 20:23:50 +0200

    axios imported

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,24 @@
 <script>
-
+import axios from 'axios';
+export default {
+  data() {
+    return {
+      title: "BOOLFOLIO FRONTEND"
+    };
+  }
+};
 </script>
 
 <template>
+  <div class="container">
+    <h1>{{ title }}</h1>
+  </div>
+
 
 
 </template>
 
 
-<style></style>
+<style lang="scss">
+@import '/src/scss/general.scss';
+</style>
