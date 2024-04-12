0b0a816ae8fb6e8e300f6e79866fa1b0b2fe8171
DemetrioPadre <demetriopadre@gmail.com>
2024-04-12 22:03:53 +0200

    add projectlist

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,48 @@
 <script>
 import axios from 'axios';
+import { api, store } from './store';
+import AppHeader from './components/AppHeader.vue';
+
 export default {
   data() {
     return {
-      title: "BOOLFOLIO FRONTEND"
+      store,
+      title: "BOOLFOLIO FRONTEND",
+
+
+
     };
+  },
+
+  components: {
+    AppHeader
+  },
+  created() {
+    axios.get(api.baserUrl + 'project').then((response) => {
+      store.projects = response.data.data;
+    });
   }
 };
 </script>
 
 <template>
-  <div class="container">
+  <app-header :title="title" />
+
+
+  <div class="container mt-5">
     <h1>{{ title }}</h1>
+
+
+    <div v-for="project in store.projects">
+      <ul>
+        <li><strong>ID:</strong>{{ project.id }}</li>
+        <li>
+          <strong>Title:</strong>{{ project.title }}
+        </li>
+        <li> <strong>Contenuto:</strong>{{ project.content }}
+        </li>
+      </ul>
+    </div>
   </div>
 
 
