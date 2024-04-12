cd022d6d6170eb12550e31aeb99fd2d6de25fcec
DemetrioPadre <demetriopadre@gmail.com>
2024-04-12 22:32:58 +0200

    milestone 5 finished

--- src/components/ProjectList.vue
+++ src/components/ProjectList.vue
@@ -1,12 +1,14 @@
 <script>
 import { api, store } from '../store';
 import axios from 'axios';
+import ProjectCard from './ProjectCard.vue';
 export default {
     data() {
         return {
             store
         };
     },
+    components: { ProjectCard },
     created() {
         axios.get(api.baserUrl + 'project').then((response) => {
             store.projects = response.data.data;
@@ -18,17 +20,7 @@ export default {
 <template>
 
     <div class="row row-cols-4 g-3">
-        <div class="col" v-for="project in store.projects">
-            <div class="card h-100 mb-5">
-                <img v-if="project.image" :src="project.image" class="card-img-top">
-                <div class="card-body">
-                    <p class="card-text">{{ project.id }}</p>
-                    <h5 class="card-title">{{ project.title }}</h5>
-                    <p class="card-text">{{ project.content }}</p>
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
-                </div>
-            </div>
-        </div>
+        <project-card v-for="project in store.projects" :project="project" />
     </div>
 
 </template>
